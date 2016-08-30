dc16f32bf7fd452d353ee7b0ef8c9ec0133cd4c7
Martin Lescano <tincholescano2@gmail.com>
2016-08-30 09:34:56 -0300

    Add files via upload

---
+++ Diagnostico.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Consultorio;
+
+/**
+ *
+ * @author emilio
+ */
+public class Diagnostico {
+    public String diagnostico;
+    public String prioridad;
+
+    public String getDiagnostico() {
+        return diagnostico;
+    }
+
+    public void setDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+
+    public String getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(String prioridad) {
+        this.prioridad = prioridad;
+    }
+    
+    
+    
+}
